c9984adbd2d7ee110b634e91f5e64361d1ae986c
Francesca Moscatelli <f.moscatelli@nextworks.it>
2020-05-12 17:01:55 +0200

    Ticketing link added in all pages

--- eve-catalogue-gui/src/app/environments/environments.ts
+++ eve-catalogue-gui/src/app/environments/environments.ts
@@ -4,13 +4,13 @@
 ​
 export const environment = {
     production: false,
-    portalBaseUrl: 'http://10.50.80.4:8082/portal/catalogue/',
-    lcmBaseUrl: 'http://10.50.80.4:8084/portal/elm/',
-    rbacBaseUrl: 'http://10.50.80.3:8090/portal/rbac/',
+    portalBaseUrl: 'http://10.50.80.18:8082/portal/catalogue/',
+    lcmBaseUrl: 'http://10.50.80.18:8084/portal/elm/',
+    rbacBaseUrl: 'http://10.50.80.13:8090/portal/rbac/',
     iwlBaseUrl: 'http://10.50.80.10:8083/',
-    tsbBaseUrl: 'http://10.5.7.11:9090/portal/tsb/',
+    tsbBaseUrl: 'http://10.50.80.3:9090/portal/tsb/',
     ibnBaseUrl: 'http://10.50.80.36:8080/Intent/IntentPage.jsp',
-    apiUrl: 'http://10.50.80.4',
+    apiUrl: 'http://10.50.80.18',
     dcsBaseUrl: 'http://10.50.80.25/portal/dcs/dashboard/',
     backServerUrl: ''
 };
