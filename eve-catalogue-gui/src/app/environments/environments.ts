f5faef62345a2fd308035f17621795a930aea9b9
Elian Kraja <e.kraja@nextworks.it>
2020-05-31 19:01:21 +0200

    Applying updated on ticketing system

--- eve-catalogue-gui/src/app/environments/environments.ts
+++ eve-catalogue-gui/src/app/environments/environments.ts
@@ -3,16 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 ​
 export const environment = {
-    production: false,
-    portalBaseUrl: 'http://10.50.80.18:8082/portal/catalogue/',
-    lcmBaseUrl: 'http://10.50.80.18:8084/portal/elm/',
-    rbacBaseUrl: 'http://10.50.80.13:8090/portal/rbac/',
-    iwlBaseUrl: 'http://10.50.80.10:8083/',
-    tsbBaseUrl: 'http://10.50.80.13:9090/portal/tsb/',
-    ibnBaseUrl: 'http://10.50.80.36:8080/Intent/IntentPage.jsp',
-    apiUrl: 'http://10.50.80.18',
-    dcsBaseUrl: 'http://10.50.80.25/portal/dcs/dashboard/',
-    backServerUrl: ''
+  production: false,
+  portalBaseUrl: 'http://10.5.7.10:8082/portal/catalogue/',
+  lcmBaseUrl: 'http://10.5.7.10:8084/portal/elm/',
+  rbacBaseUrl: 'http://10.5.7.11:8090/portal/rbac/',
+  iwlBaseUrl: 'http://10.3.3.30:8083/',
+  tsbBaseUrl: 'http://10.5.7.11:9090/portal/tsb/',
+  ibnBaseUrl: 'http://10.5.7.13:8080/Intent/IntentPage.jsp',
+  apiUrl: 'http://10.5.7.10',
+  dcsBaseUrl: 'http://10.5.7.12:8080/portal/dcs/dashboard/',
+  backServerUrl: ''
 };
 ​
 /*
