7bdebb58eae8af900a23d4f384582f54ef5b1e4d
Elian Kraja <e.kraja@nextworks.it>
2020-06-24 12:58:31 +0200

    fix on page visualization

--- eve-catalogue-gui/src/app/environments/environments.5tonic.prod.ts
+++ eve-catalogue-gui/src/app/environments/environments.5tonic.prod.ts
@@ -7,11 +7,12 @@ export const environment = {
     portalBaseUrl: 'https://portal.5g-eve.eu/portal/catalogue/',
     lcmBaseUrl: 'https://portal.5g-eve.eu/portal/elm/',
     rbacBaseUrl: 'https://portal.5g-eve.eu/portal/rbac/',
-    iwlBaseUrl: 'https://portal.5g-eve.ue/iwl/cat/',
+    iwlBaseUrl: 'https://portal.5g-eve.eu/iwl/cat/',
     tsbBaseUrl: 'https://portal.5g-eve.eu/portal/tsb/',
     ibnBaseUrl: 'https://portal.5g-eve.eu/ibn/Intent/IntentPage.jsp',
     apiUrl: 'https://portal.5g-eve.eu/',
-    dcsBaseUrl: 'https://portal.5g-eve.ue/portal/dcs/dashboard/',
+    dcsBaseUrl: 'https://portal.5g-eve.eu/portal/dcs/dashboard/',
+    fsBaseUrl: 'https://portal.5g-eve.eu/portal/fs/',
     backServerUrl: ''
 };
 ​
