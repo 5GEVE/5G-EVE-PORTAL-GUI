4248077a75842d3fab2f8a6c2747086b6c58ee19
Elian Kraja <e.kraja@nextworks.it>
2020-11-05 11:30:22 +0100

    Fix on footer links. Added documentation link

--- eve-catalogue-gui/src/app/navigation/footer/footer.component.ts
+++ eve-catalogue-gui/src/app/navigation/footer/footer.component.ts
@@ -19,11 +19,11 @@ export class FooterComponent implements OnInit {
     {"title":"5G EVE portal",
     "link":"https://portal.5g-eve.eu",
     "context":"Portal to to design, manage and monitor experiments over the 5G EVE end-to--end facility infrastructures."},
-    {"title":"5G EVE Wiki",
-    "link":"http://wiki.5g-eve.eu",
+    {"title":"5G EVE Documentation",
+    "link":"https://www.it.uc3m.es/jgr/5G-EVE/5G_EVE_Portal_User_Manual.pdf",
     "context":"Documentation for experimenters and developers operating on 5G EVE."},
     {"title":"Report Issues",
-    "link":"https://portal.5g-eve.eu/bugzilla",
+    "link":"https://portal.5g-eve.eu/#/tickets",
     "context":"Issue tracker for 5G EVE experimenters."}
   ];
   ngOnInit(): void {
